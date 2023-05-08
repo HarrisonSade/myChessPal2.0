549aa994bd0e0df1ce3db0c0559745bf610d5d73
harrisonsade <harrisons2025@headroyce.org>
2023-05-08 10:35:50 -0700

    new stuff

--- src/main/java/com/example/mychesspal2_0/dutch.java
+++ src/main/java/com/example/mychesspal2_0/dutch.java
@@ -41,6 +41,7 @@ public class dutch extends BorderPane {
             }
         }
 
+
         setCenter(gridPane);
     }
 }
--- src/main/java/com/example/mychesspal2_0/french.java
+++ src/main/java/com/example/mychesspal2_0/french.java
@@ -2,12 +2,17 @@ package com.example.mychesspal2_0;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class french extends BorderPane {
+    private static final int BOARD_SIZE = 8;
 
     public french(){
         Button backBtn = new Button("Back");
@@ -24,5 +29,17 @@ public class french extends BorderPane {
                 s.setTitle("Defense Lessons");
             }
         });
+
+        GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+
+        for (int row = 0; row < BOARD_SIZE; row++) {
+            for (int col = 0; col < BOARD_SIZE; col++) {
+                Rectangle rect = new Rectangle(75, 75, (row + col) % 2 == 0 ? Color.WHITE : Color.LIGHTGRAY);
+                gridPane.add(rect, col, row);
+            }
+        }
+
+        setCenter(gridPane);
     }
 }
