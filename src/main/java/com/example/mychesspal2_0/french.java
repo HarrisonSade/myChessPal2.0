3f334d88a08520cb9430df1ea49d03d15d0bda9a
harrisonsade <harrisons2025@headroyce.org>
2023-05-08 10:02:45 -0700

    new stuff

---
+++ src/main/java/com/example/mychesspal2_0/french.java
@@ -0,0 +1,28 @@
+package com.example.mychesspal2_0;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class french extends BorderPane {
+
+    public french(){
+        Button backBtn = new Button("Back");
+        backBtn.setMaxSize(75, 50);
+        this.setTop(backBtn);
+
+        backBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                defenseLessons defense = new defenseLessons();
+                Stage s = (Stage) french.this.getScene().getWindow();
+                Scene homeScene = new Scene(defense, s.getWidth(), s.getHeight());
+                s.setScene(homeScene);
+                s.setTitle("Defense Lessons");
+            }
+        });
+    }
+}
---
+++ src/main/java/com/example/mychesspal2_0/kingsGambit.java
@@ -0,0 +1,28 @@
+package com.example.mychesspal2_0;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class kingsGambit extends BorderPane {
+
+    public kingsGambit() {
+        Button backBtn = new Button("Back");
+        backBtn.setMaxSize(75, 50);
+        this.setTop(backBtn);
+
+        backBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                openingLessons openingLessons = new openingLessons();
+                Stage s = (Stage) kingsGambit.this.getScene().getWindow();
+                Scene homeScene = new Scene(openingLessons, s.getWidth(), s.getHeight());
+                s.setScene(homeScene);
+                s.setTitle("Opening Lessons");
+            }
+        });
+    }
+}
