62f1d9e75a8ce7b820e0af1f3a96b55b824e8739
jared <jaredc2024@headroyce.org>
2023-05-22 09:59:22 -0700

    push for harrison's error

--- src/main/java/com/example/mychesspal2_0/caro.java
+++ src/main/java/com/example/mychesspal2_0/caro.java
@@ -5,8 +5,11 @@ import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -14,6 +17,8 @@ import javafx.stage.Stage;
 public class caro extends BorderPane {
 
     private static final int BOARD_SIZE = 8;
+    Button[][] squares = new Button[BOARD_SIZE][BOARD_SIZE];
+
     public caro(){
         Button backBtn = new Button("Back");
         backBtn.setMaxSize(75, 50);
@@ -33,10 +38,61 @@ public class caro extends BorderPane {
         GridPane gridPane = new GridPane();
         gridPane.setAlignment(Pos.CENTER);
 
+        //new for loop for the board
         for (int row = 0; row < BOARD_SIZE; row++) {
             for (int col = 0; col < BOARD_SIZE; col++) {
-                Rectangle rect = new Rectangle(75, 75, (row + col) % 2 == 0 ? Color.WHITE : Color.LIGHTGRAY);
-                gridPane.add(rect, col, row);
+                Button button = new Button();
+                button.setPrefSize(75,75);
+                button.setStyle("-fx-background-color: " + ((row + col) % 2 == 0 ? "white" : "lightgray"));
+                squares[row][col] = button;
+
+                //Rectangle rect = new Rectangle(75, 75, (row + col) % 2 == 0 ? Color.WHITE : Color.LIGHTGRAY);
+                //squares[row][col] = rect;
+
+                // Create ImageView and add it to a StackPane
+                StackPane stackPane = new StackPane();
+                stackPane.getChildren().add(button);
+                ImageView imageView = new ImageView();
+                imageView.setFitWidth(60);
+                imageView.setFitHeight(60);
+                stackPane.getChildren().add(imageView);
+
+                // Set the image of the ImageView to the corresponding chess piece
+                if (row == 0 && (col == 0 || col == 7)) {
+                    imageView.setImage(new Image("white_rook.png"));
+                } else if (row == 0 && (col == 1 || col == 6)) {
+                    imageView.setImage(new Image("white_knight.png"));
+                } else if (row == 0 && (col == 2 || col == 5)) {
+                    imageView.setImage(new Image("white_bishop.png"));
+                } else if (row == 0 && col == 3) {
+                    imageView.setImage(new Image("white_king.png"));
+                } else if (row == 0 && col == 4) {
+                    imageView.setImage(new Image("white_queen.png"));
+                } else if (row == 1) {
+                    imageView.setImage(new Image("white_pawn.png"));
+                } else if (row == 6) {
+                    imageView.setImage(new Image("black_pawn.png"));
+                } else if (row == 7 && (col == 0 || col == 7)) {
+                    imageView.setImage(new Image("black_rook.png"));
+                } else if (row == 7 && (col == 1 || col == 6)) {
+                    imageView.setImage(new Image("black_knight.png"));
+                } else if (row == 7 && (col == 2 || col == 5)) {
+                    imageView.setImage(new Image("black_bishop.png"));
+                } else if (row == 7 && col == 3) {
+                    imageView.setImage(new Image("black_king.png"));
+                } else if (row == 7 && col == 4) {
+                    imageView.setImage(new Image("black_queen.png"));
+                }
+
+                int testRow = row;
+                int testCol = col;
+                button.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        System.out.println("Button Clicked: Row = " + testRow + " Col = " + testCol);
+                    }
+                });
+                gridPane.add(stackPane, col, row);
             }
         }
 
